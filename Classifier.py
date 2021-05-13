5b10e4a2d6a07d5d873a905bc8c228166bca11f5
BodduRaviteja <65815640+BodduRaviteja@users.noreply.github.com>
2021-05-13 18:10:38 +0200

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,3 +1,4 @@
+# Import Modules required 
 import git
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,11 +15,13 @@ import pickle
 import pandas as pd
 import cv2
 
-#path = 'C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data'
-#clone = 'https://bitbucket.org/jadslim/german-traffic-signs'
-#g = git.Git(path).clone(clone)
+#Git cloning
+path = 'Enter the path where these images have to be saved'
+clone = 'https://bitbucket.org/jadslim/german-traffic-signs'
+g = git.Git(path).clone(clone)
 np.random.seed(0)
 
+#Loading the images into three sets
 with open('C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data\\german-traffic-signs\\train.p', 'rb') as f:
     train_data = pickle.load(f)
 with open('C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data\\german-traffic-signs\\valid.p', 'rb') as f:
@@ -26,18 +29,21 @@ with open('C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data\\german-traffic-sign
 with open('C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data\\german-traffic-signs\\test.p', 'rb') as f:
     test_data = pickle.load(f)
 
-#print(type(train_data)) # we will get a dictionary which has features and lables
+print(type(train_data)) # we will get a dictionary which has features and lables
+
+# Unpack into features and labels
 X_train, y_train = train_data['features'], train_data['labels']
 #features corresponds to the trainig images in the pix values
 X_test, y_test = test_data['features'], test_data['labels']
 X_val, y_val = valid_data['features'], valid_data['labels']
 
-#plt.imshow(X_train[1000])
+plt.imshow(X_train[1000])#Check for an arbitary image
 
 print(X_train.shape)
 print(X_val.shape)
 print(X_test.shape)
 
+# To further proceed just pre-checking whether i have same no. of images and labels and its size
 assert (X_train.shape[0] == y_train.shape[0]), 'The no. of images is not equal to no. of lables'
 assert (X_val.shape[0] == y_val.shape[0]), 'The no. of images is not equal to no. of lables'
 assert (X_test.shape[0] == y_test.shape[0]), 'The no. of images is not equal to no. of lables'
@@ -46,20 +52,21 @@ assert(X_train.shape[1:] == (32,32,3)), 'The size of the images are not 32x32x3'
 assert(X_val.shape[1:] == (32,32,3)), 'The size of the images are not 32x32x3'
 assert(X_test.shape[1:] == (32,32,3)), 'The size of the images are not 32x32x3'
 
+#Load the Signnames which have classId with its corresponding Sign description
 data = pd.read_csv('C:\\Users\\HP\\PycharmProjects\\IP\\Traffic Data\\german-traffic-signs\\signnames.csv')
 print(data)
 
-
+#Displaying first 5 images of each class (total 43)
 num_of_samples = []
 
 cols = 5
 num_classes = 43
 
 fig, axs = plt.subplots(nrows=num_classes, ncols=cols, figsize=(10, 50))
-fig.tight_layout()
+fig.tight_layout() #to prevent overlapping of images
 
 for i in range(cols):
-    for j, row in data.iterrows(): #it has two arguements like indes & Series(data value)
+    for j, row in data.iterrows(): #it has two arguements like indices & Series(data value)
         x_selected = X_train[y_train == j]
         axs[j][i].imshow(x_selected[random.randint(0, (len(x_selected) - 1)), :, :], cmap=plt.get_cmap('gray'))
         axs[j][i].axis("off")
@@ -67,10 +74,13 @@ for i in range(cols):
             axs[j][i].set_title(str(j) + '-' + row["SignName"])
             num_of_samples.append(len(x_selected))
 
+
+            
+#Preprocessing and Data Augmentation of data
+from keras.preprocessing.image import ImageDataGenerator            
 def grayscale(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return image
-
 
 def equalize(image):
     image = cv2.equalizeHist(image) #only accept grayscale image as input
@@ -90,11 +100,7 @@ X_train = X_train.reshape(34799, 32, 32, 1)
 X_val = X_val.reshape(4410, 32, 32, 1)
 X_test = X_test.reshape(12630, 32, 32, 1)
 
-from keras.preprocessing.image import ImageDataGenerator
-
 datagen = ImageDataGenerator(width_shift_range = 0.1,
-                   #its 10% of the image width
-                # if its more than 1 then would consider it as shift in image pixels
                    height_shift_range = 0.1,
                    zoom_range = 0.2,
                    shear_range = 0.1,
@@ -121,7 +127,7 @@ y_test = to_categorical(y_test, 43)
 y_val = to_categorical(y_val, 43)
 
 
-
+#Neural Network
 def LeNet_model():
     model = Sequential()
     model.add(Conv2D(30, (5, 5), input_shape=(32, 32, 1), activation='relu', strides= 1))
@@ -136,18 +142,20 @@ def LeNet_model():
     return model
 
 model = LeNet_model()
-print(model.summary())
+print(model.summary())#Show the detailed summary of our Architecture
 
 #model.fit_generator is used to parallely run the image generator along with the model
 model.fit_generator(datagen.flow(X_train, y_train, batch_size = 50), steps_per_epoch = 2000, epochs = 10, validation_data = (X_val, y_val), shuffle= 1 )
 #model.fit(X_train, y_train, epochs= 10, validation_data = (X_val, y_val), batch_size= 400, verbose= 1, shuffle= 1)
 
-
+#Score Evaluation 
 score = model.evaluate(X_test, y_test, verbose=0)
 print(type(score))
-print('Test score is:' , score[0])
+print('Test score is:', score[0])
 print('Test accuracy is:', score[1])
 
+
+# testing the model
 import requests
 from PIL import Image
 
@@ -156,8 +164,7 @@ r = requests.get(url, stream=True)
 img = Image.open(r.raw)
 plt.imshow(img, cmap=plt.get_cmap('gray'))
 
-# Preprocess image
-
+# Preprocessing Test image
 img = np.asarray(img)
 img = cv2.resize(img, (32, 32))
 img = preprocessing(img)
@@ -165,11 +172,7 @@ plt.imshow(img, cmap=plt.get_cmap('gray'))
 print(img.shape)
 
 # Reshape reshape
-
 img = img.reshape(1, 32, 32, 1)
 
-# Test image
 print("predicted sign: " + str(np.argmax(model.predict(img))))
-
-
 plt.show()
